e13b7c3230b3693f7aa7161658124c2f0cc378a6
abdulfalaq5 <abdulfalaq5@gmail.com>
2026-04-15 10:22:29 +0700

    add: sync location

--- src/modules/locations/controller.js
+++ src/modules/locations/controller.js
@@ -2,7 +2,7 @@ const service = require('./service');
 const { baseResponse } = require('../../utils');
 
 /**
- * Get locations list
+ * Get locations list (dari DB)
  */
 const getList = async (req, res) => {
   try {
@@ -24,6 +24,30 @@ const getList = async (req, res) => {
   }
 };
 
+/**
+ * Sync locations dari bridge API
+ */
+const sync = async (req, res) => {
+  try {
+    const result = await service.syncLocationsList(req.body);
+    return baseResponse(res, {
+      data: {
+        success: true,
+        data: result,
+        message: 'Data locations berhasil di-sync dari bridge API'
+      }
+    });
+  } catch (error) {
+    const statusCode = error.statusCode || 500;
+    return res.status(statusCode).json({
+      success: false,
+      message: error.message || 'Internal Server Error',
+      errors: error.errors || error
+    });
+  }
+};
+
 module.exports = {
-  getList
+  getList,
+  sync
 };
--- src/modules/locations/index.js
+++ src/modules/locations/index.js
@@ -5,13 +5,24 @@ const { verifyToken } = require('../../middlewares');
 
 /**
  * @route   POST /api/netsuite/locations/get-list
- * @desc    Get locations from bridge API
+ * @desc    Get locations dari database lokal (bridge_sanbox)
  * @access  Private
  */
 router.post(
   '/get-list',
   verifyToken,
   controller.getList
+);
+
+/**
+ * @route   POST /api/netsuite/locations/sync
+ * @desc    Sync locations dari bridge API
+ * @access  Private
+ */
+router.post(
+  '/sync',
+  verifyToken,
+  controller.sync
 );
 
 module.exports = router;
