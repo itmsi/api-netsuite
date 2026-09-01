75e04b61c52a0718ae04b34a53b7b8188808e686
abdulfalaq5 <abdulfalaq5@gmail.com>
2026-09-01 14:26:39 +0700

    fix: update proses delete file atatchment di po dan to

--- src/modules/attach_file/index.js
+++ src/modules/attach_file/index.js
@@ -1,38 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('./controller');
-const { verifyToken } = require('../../middlewares');
-const multer = require('multer');
+const controller = require("./controller");
+const { verifyToken } = require("../../middlewares");
+const multer = require("multer");
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: parseInt(process.env.UPLOAD_MAX_SIZE || '52428800') }
+  limits: { fileSize: parseInt(process.env.UPLOAD_MAX_SIZE || "52428800") },
 });
 
-router.post(
-  '/get',
-  verifyToken,
-  controller.getList
-);
+router.post("/get", verifyToken, controller.getList);
 
-router.post(
-  '/',
-  verifyToken,
-  upload.single('file'),
-  controller.create
-);
+router.post("/", verifyToken, upload.single("file"), controller.create);
 
-router.put(
-  '/:id',
-  verifyToken,
-  upload.single('file'),
-  controller.update
-);
+router.put("/:id", verifyToken, upload.single("file"), controller.update);
 
-router.delete(
-  '/:id/:netsuite_id',
-  verifyToken,
-  controller.destroy
-);
+router.delete("/:id/:netsuite_id/:type", verifyToken, controller.destroy);
 
 module.exports = router;
